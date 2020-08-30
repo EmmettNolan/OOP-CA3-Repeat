be39ad4d457fbd9fed6ad7adb657c920b6934ba3
S00188098 <S00188098@mail.itsligo.ie>
2020-08-31 00:44:45 +0100

    Changes

--- CA3/CA3/MainWindow.xaml.cs
+++ CA3/CA3/MainWindow.xaml.cs
@@ -20,9 +20,87 @@ namespace CA3
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Player> allPlayers = new List<Player>();
+        List<Player> selectedPlayers = new List<Player>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Sorting by first name then second name.
+            allPlayersLB.ItemsSource = allPlayers.OrderBy(s => s.Name).ThenBy(s => s.Surname);
+
+            // Creating the randomNames
+            string[] firstNames = { "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily", "Emma", "Grace", "Hannah", "Harry", "Jack", "James", "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie" };
+            string[] lastNames = { "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh", "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien", "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh" };
+
+            Random rng = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                int number = rng.Next(0, 20);
+                string Name = firstNames[number];
+                number = rng.Next(0, 20);
+                string Surname = lastNames[number];
+            }
+
+            // Setting up the contents of the ComboBox
+            formationsCbx.ItemsSource = new string[] { "4-4-2", "4-3-3", "4-5-1" };
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //Figure out which player is selected on screen //
+            Player selectedPlayer = allPlayersLB.SelectedItem as Player;
+
+            //Null Checker
+            if (selectedPlayer != null)
+            {
+
+                //Move the item which is clicked from the left listbox to the right listbox
+                allPlayers.Remove(selectedPlayer);
+                selectedPlayers.Add(selectedPlayer);
+            }
+        }
+
+        private void RefreshingScreen()
+        {
+            allPlayersLB.ItemsSource = null;
+            allPlayersLB.ItemsSource = allPlayers;
+
+            selectedPlayersLB.ItemsSource = null;
+            selectedPlayersLB.ItemsSource = selectedPlayers;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Figure out which player is selected on screen //
+            Player selectedPlayers = allPlayersLB.SelectedItem as Player;
+
+            //Null Checker
+            if (selectedPlayers != null)
+            {
+                //Move the item which is clicked from the left listbox to the right listbox
+                allPlayers.Add(selectedPlayers);
+                allPlayers.Remove(selectedPlayers);
+            }
+        }
+
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            Player selected = allPlayersLB.SelectedItem as Player;
+            if (selected != null)
+            {
+                spacesBx.Text = selected.Spaces;
+            }
+        }
+
+        private void allPlayersLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
--- CA3/CA3/Player.cs
+++ CA3/CA3/Player.cs
@@ -10,6 +10,42 @@ namespace CA3
     {
         public enum Position { Goalkeeper, Defender, Midfielder, Forward }
 
+        // Properties //
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Position PerferredPosition { get; set; }
+        public Position PlayerPosition { get; set; }
+        public string Spaces { get; set; }
+        public DateTime DOB { get; set; }
 
+        private int _age;
+
+
+        public int Age
+        {
+            get
+            {
+                _age = DateTime.Now.Year - DOB.Year;
+                if (DOB.DayOfYear > DateTime.Now.DayOfYear)
+                    _age--;
+                return _age;
+            }
+        }
+
+        public Player(string name, string surname, Position preferedPosition)
+        {
+            Name = name;
+            Surname = surname;
+
+            PlayerPosition = preferedPosition;
+
+
+        }
+
+        // Methods //
+        public override string ToString()
+        {
+            return $"{Name} {Surname} {DOB} {PlayerPosition}";
+        }
     }
 }
